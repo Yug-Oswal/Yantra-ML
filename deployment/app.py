8da385a1589f62953fa3442553807b2b10aa6358
Yug-Oswal <yoswal071@gmail.com>
2024-04-13 18:28:41 +0530

    update: ner model initial fine-tuning

--- deployment/app.py
+++ deployment/app.py
@@ -1,24 +1,31 @@
 from flask import Flask, request
 import tensorflow as tf 
-import keras_nlp
-import tensorflow_hub as hub
+import os
+# import keras_nlp
+# import tensorflow_hub as hub
 
-preprocessor = keras_nlp.models.DistilBertPreprocessor.from_preset(
-    None,
-    sequence_length=256,
-)
-encoder = keras_nlp.models.DistilBertBackbone.from_preset(
-    None
-)
-encoder.trainable = True
-txt = tf.keras.layers.Input(shape=(), dtype=tf.string)
-x = preprocessor(txt)
-x = encoder(x)
-x = tf.keras.layers.GlobalAveragePooling1D()(x)
-x = tf.keras.layers.Dropout(0.1)(x)
-x = tf.keras.layers.Dense(Y_train.shape[1], activation='softmax')(x)
-model = tf.keras.Model(inputs=[txt], outputs=x)
-model.load_weights('../DistilBERT/EmotionExtractor')
+os['TF_ENABLE_ONEDNN_OPTS'] = 0
+
+# preprocessor = keras_nlp.models.DistilBertPreprocessor.from_preset(
+#     "distil_bert_base_en_uncased",
+#     sequence_length=256,
+# )
+# encoder = keras_nlp.models.DistilBertBackbone.from_preset(
+#     "distil_bert_base_en_uncased"
+# )
+# encoder.trainable = True
+# txt = tf.keras.layers.Input(shape=(), dtype=tf.string)
+# x = preprocessor(txt)
+# x = encoder(x)
+# x = tf.keras.layers.GlobalAveragePooling1D()(x)
+# x = tf.keras.layers.Dropout(0.1)(x)
+# x = tf.keras.layers.Dense(Y_train.shape[1], activation='softmax')(x)
+# model = tf.keras.Model(inputs=[txt], outputs=x)
+# model.load_weights('../DistilBERT/EmotionExtractor')
+
+model = tf.saved_model.load('../EmotionExtractor')
+# tf.config.set_visible_devices([], 'GPU')  
+predictor = model.signatures["serving_default"]
 
 app = Flask(__name__)
 
